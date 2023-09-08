12ced4f5e14a9764d316c6e508142547262d0ab3
751200 <357993262@qq.com>
2023-09-08 10:16:44 +0800

    day03_持久化配置完成

--- _03_wiki/src/main/java/com/gec/_03_wiki/controller/EbookController.java
+++ _03_wiki/src/main/java/com/gec/_03_wiki/controller/EbookController.java
@@ -1,7 +1,12 @@
 package com.gec._03_wiki.controller;
 
-import com.gec._03_wiki.mapper.EbookMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.gec._03_wiki.pojo.CommonResp;
 import com.gec._03_wiki.pojo.Ebook;
+import com.gec._03_wiki.pojo.EbookReq;
+import com.gec._03_wiki.pojo.EbookResp;
+import com.gec._03_wiki.service.EbookService;
+import com.gec._03_wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +17,30 @@ import java.util.List;
 public class EbookController {
 
     @Autowired
-    private  EbookMapper ebookmapper;
+    private EbookService ebookService;
 
     @GetMapping("/ebookAll")
-    public List<Ebook> getAll(){
-        List<Ebook> list = ebookmapper.selectList(null);
-        return list;
+    public CommonResp getAll(){
+        List<Ebook> list = ebookService.list();
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/getEbookByName")
+    public CommonResp getEbookByName(EbookReq req){
+        QueryWrapper<Ebook> wrapper = new QueryWrapper<>();
+        wrapper.like("name",req.getName());
+        List<Ebook> list = ebookService.list(wrapper);
+
+//        ArrayList<EbookResp> ebookResps = new ArrayList<>();
+//        for (Ebook ebook : list) {
+//            EbookResp copy = CopyUtil.copy(ebook, EbookResp.class);
+//            ebookResps.add(copy);
+//        }
+        List<EbookResp> ebookResps = CopyUtil.copyList(list, EbookResp.class);
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        resp.setContent(ebookResps);
+        return resp;
     }
 }
---
+++ _03_wiki/src/main/java/com/gec/_03_wiki/pojo/EbookReq.java
@@ -0,0 +1,9 @@
+package com.gec._03_wiki.pojo;
+
+import lombok.Data;
+
+@Data
+public class EbookReq {
+    private Integer id;
+    private String name;         //名称
+}
